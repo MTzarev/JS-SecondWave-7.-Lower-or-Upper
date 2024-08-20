8df6975473d9866e9296c168ae32dc0f51ff480f
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-20 18:53:51 +0300

    Add files via upload

---
+++ 7. Lower or Upper.js
@@ -0,0 +1,8 @@
+function lowerOrUpper(letter) {
+    if (letter !== letter.toUpperCase()) {
+        console.log(`lower-case`);
+    } else {
+        console.log(`upper-case`);
+    }
+}
+lowerOrUpper(`L`);
